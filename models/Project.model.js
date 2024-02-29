2ddd24966413b8a16c8f6778293cb6945af90b98
Pauline Cvt <paulinecvt2021@gmail.com>
2024-02-29 09:24:48 +0100

    improved model + put and delete

--- models/Project.model.js
+++ models/Project.model.js
@@ -2,9 +2,34 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const projectSchema = new Schema({
-  title: String,
-  description: String,
-});
+  title: {
+    type: String,
+    required: true,
+    default: "New Project",
+  },
+  description: {
+    type: String
+  },
+  tasks: {
+    type: [String],
+    default: []
+  },
+  startDate: {
+    type: Date,
+    default: Date.now
+  },
+  endDate: {
+    type: Date
+  },
+  collaborators: {
+    type: [String]
+  },
+  documentation: {
+    type: [String]
+  }
+  }
+
+);
 
 
 module.exports = model("Project", projectSchema);
